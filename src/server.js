fce255d0b1c7db484c8731d26e09d765d5191c47
Kadu Lisboa <kadulisboa301@icloud.com>
2023-02-08 23:09:14 -0300

    (Ignite NodeJS - Fundamentos Node ) Usando JSON com Stream

--- src/server.js
+++ src/server.js
@@ -2,29 +2,49 @@ import http from 'node:http';
 
 const users = [];
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const {method, url} = request;
+
+  const buffers = [];
+
+  for await ( const chunk of request ) {
+    buffers.push(chunk);
+  }
+
+  try {
+    
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+
+  } catch {
+
+    request.body = null;
+
+  }
+
+  
   
   if( url === '/users' && method === 'GET') {
-    console.table([{"URL": url, "METHOD" : method, "ACTION": "Listagem de Usuários" }]);
+    // console.table([{"URL": url, "METHOD" : method, "ACTION": "Listagem de Usuários" }]);
+    
     return response
             .setHeader('Content-Type', 'application/json')
             .end(JSON.stringify(users));
   }
 
   if( url === '/users' && method === 'POST') {
-    console.table([{"URL": url, "METHOD" : method, "ACTION": "Criação de Usuários" }]);
-    
+    const { name, email } = request.body;
+    // console.table([{"URL": url, "METHOD" : method, "ACTION": "Criação de Usuários" }]);
+
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'john@doe.com',
-    })
+      name,
+      email,
+    });
 
     return response.writeHead(201).end();
   }
 
-  console.table([{"URL": url, "METHOD" : method, "ACTION": "" }]);
+  // console.table([{"URL": url, "METHOD" : method, "ACTION": "" }]);
   return response.writeHead(404).end();
 })
 
