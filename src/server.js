0db713d7ee747f1c2a8574d4c7fd1fee0cbd9c90
Kadu Lisboa <kadulisboa301@icloud.com>
2023-02-15 23:12:18 -0300

    (Ignite NodeJS - Fundamentos Node ) Rotas da aplicacao

--- src/server.js
+++ src/server.js
@@ -1,50 +1,24 @@
 import http from 'node:http';
-
-const users = [];
+import { json } from './middlewares/json.js';
+import { routes } from './routes.js';
 
 const server = http.createServer(async (request, response) => {
   const {method, url} = request;
 
-  const buffers = [];
-
-  for await ( const chunk of request ) {
-    buffers.push(chunk);
-  }
-
-  try {
-    
-    request.body = JSON.parse(Buffer.concat(buffers).toString());
-
-  } catch {
-
-    request.body = null;
-
-  }
-
+  await json(request, response);
   
-  
-  if( url === '/users' && method === 'GET') {
-    // console.table([{"URL": url, "METHOD" : method, "ACTION": "Listagem de Usuários" }]);
-    
-    return response
-            .setHeader('Content-Type', 'application/json')
-            .end(JSON.stringify(users));
+  const route = routes.find( route => {
+    return route.method === method && route.path.test(url);
+  })
+
+  if ( route) {
+    const routeParams = request.url.match(route.path)
+
+    request.params = {...routeParams.groups};
+
+    return route.handler(request, response);
   }
 
-  if( url === '/users' && method === 'POST') {
-    const { name, email } = request.body;
-    // console.table([{"URL": url, "METHOD" : method, "ACTION": "Criação de Usuários" }]);
-
-    users.push({
-      id: 1,
-      name,
-      email,
-    });
-
-    return response.writeHead(201).end();
-  }
-
-  // console.table([{"URL": url, "METHOD" : method, "ACTION": "" }]);
   return response.writeHead(404).end();
 })
 
